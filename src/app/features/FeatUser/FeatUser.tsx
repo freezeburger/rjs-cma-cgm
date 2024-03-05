3f531a318fb94fae529437ce326f037cbd099e24
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-03-05 16:41:18 +0100

    Multipple Feature

--- src/app/features/FeatUser/FeatUser.tsx
+++ src/app/features/FeatUser/FeatUser.tsx
@@ -1,15 +1,22 @@
 import { FC } from 'react';
 import { useUsers } from './hooks/use-users.hook';
 import { User } from './logic/interface';
-import { useData } from './hooks/use-data.hook';
 
-const UserList:FC<{collection:User[]}> = ({collection}) => {
-  return(
+const UserList: FC<{ collection: User[] }> = ({ collection }) => {
+  return (
     <>
-     {
-        collection.length &&
-        collection.map( user => <div key={user.id}>{user.name}</div>)
-        || 'Loading'
+      {
+        collection.map(user => <div key={user.id}>{user.name}</div>)
+      }
+    </>
+  )
+}
+
+const UserLoader: FC<{ collection: User[] }> = ({ collection }) => {
+  return (
+    <>
+      {
+        !collection.length && 'Loading'
       }
     </>
   )
@@ -19,11 +26,12 @@ interface FeatUserProps { }
 
 const FeatUser: FC<FeatUserProps> = () => {
 
-  const {users} = useUsers();
+  const { users } = useUsers();
 
   return (
     <div data-testid="FeatUser">
-      <UserList collection={users}/>
+      <UserList collection={users} />
+      <UserLoader collection={users} />
     </div>
   )
 };
---
+++ src/app/features/FeatUser/FeatUserCount.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+interface FeatUserCountProps { }
+
+const FeatUserCount: FC<FeatUserCountProps> = () => {
+
+  return (
+    <>
+      UserCount
+    </>
+  )
+};
+
+export default FeatUserCount;
